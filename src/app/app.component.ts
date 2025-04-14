b0f155ea33314f806c5693805b2bd503b2f7d858
Joao Neves <joao.m.guedes.neves@avanade.com>
2025-04-14 08:06:08 +0200

    app startup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [ButtonModule],
+  styleUrl: './app.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <div class="text-center text-4xl font-bold text-blue-500">hello world</div>
+    <p-button label="Submit" />
+  `,
 })
 export class AppComponent {
-  title = 'ava-todo';
+  title = 'ava todo';
 }
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
+      },
+    },
+  },
+  // plugins: [require("tailwindcss-primeui")],
+};
