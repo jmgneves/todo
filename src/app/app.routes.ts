e04a3bba4662a6379ebf9fa29579939ce2799a35
Joao Neves <joao.m.guedes.neves@avanade.com>
2025-04-15 17:14:30 +0200

    reorganize folder structure and add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './features/welcome/welcome.component';
+import { TodoListSignalStoreComponent } from './features/todo-list-signal-store/todo-list-signal-store.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'signal-store', component: TodoListSignalStoreComponent },
+];
